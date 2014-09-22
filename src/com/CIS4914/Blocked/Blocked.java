55ead8f3a72b3b1f1215adfbf1e65064a072112e
st3w <stew.reive@gmail.com>
2014-09-22 14:42:04 -0400

    added GameScreen class

--- src/com/CIS4914/Blocked/Blocked.java
+++ src/com/CIS4914/Blocked/Blocked.java
@@ -1,19 +1,31 @@
 package com.CIS4914.Blocked;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
 import com.CIS4914.Blocked.Screens.MainMenu;
 
 public class Blocked extends Game {
 	
 	public static final String version = "0.00.00 Alpha Initial Commit";
 	public static final String LOG = "Blocked";
-
-    MainMenu mainMenuScreen;
+	
+	private AssetManager manager;
+    private MainMenu mainMenuScreen;
 
    @Override
     public void create() {
-            mainMenuScreen = new MainMenu(this);
-            setScreen(mainMenuScreen);   
+	   	manager = new AssetManager();
+		manager.load("smiley.png", Texture.class);
+		manager.finishLoading();
+		
+        mainMenuScreen = new MainMenu(this);
+        setScreen(mainMenuScreen);   
    }
-
+   
+   @Override
+   public void render() {
+	   super.render();
+   }
 }
---
+++ src/com/CIS4914/Blocked/Screens/GameScreen.java
@@ -0,0 +1,65 @@
+package com.CIS4914.Blocked.Screens;
+
+import com.CIS4914.Blocked.Blocked;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class GameScreen implements Screen {
+
+	private Stage stage;
+	private Blocked game;
+	
+	public GameScreen(Blocked game) {
+		this.game = game;
+		stage = new Stage(new ScreenViewport());
+		
+	}
+	
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(delta);
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		stage.dispose();
+	}
+}
